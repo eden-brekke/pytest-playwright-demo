3d969642c988dd4e6d8ec700897aacd64444f940
Eden Brekke <eden.lorrai@gmail.com>
2023-01-30 12:29:49 -0800

    finished

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from playwright.sync_api import sync_playwright
 
-URL = 'https://phptravels.net/login'
-DASHBOARD_URL = 'https://phptravels.net/account/dashboard'
+url = 'https://phptravels.net/login'
+dashboard_url = 'https://phptravels.net/account/dashboard'
 
 
-EMAILIDTEXTBOX = "//input[@placeholder='Email']" # if you control f on the console and search for this xpath on the webpage it will highlight the element which is being targetted.
+email_id_textbox = "//input[@placeholder='Email']" # if you control f on the console and search for this xpath on the webpage it will highlight the element which is being targetted.
 # alternatively if you are having trouble writing an xpath to target the element you want correctly and uniquely then you can rightclick the element in the console and copy>copy xpath. It spits out an xpath thats specific to that element.
-EMAILID = "user@phptravels.com"
-PASSWORDTEXTBOX = "//input[@placeholder='Password']"
-PASSWORD = "demouser"
-SUBMIT_BUTTON = "//button[@type='submit']"
+email_id = "user@phptravels.com"
+password_text_box = "//input[@placeholder='Password']"
+password = "demouser"
+submit_button = "//button[@type='submit']"
 
 # https://phptravels.net/
 # = "//[='']"
 
-MY_BOOKINGS_BUTTON = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/bookings']"
-MY_BOOKINGS_VERIFY = "//h3[text()='My Bookings']"
-ADD_FUNDS_BUTTON = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/add_funds']"
-ADD_FUNDS_VERIFY = "//h3[text()='Add Funds']"
-PROFILE_BUTTON = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/profile']"
-PROFILE_VERIFY = "//h3[text()='Profile Information']"
-LOGOUT_BUTTON = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/logout']"
+my_bookings_button = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/bookings']"
+my_bookings_verify = "//h3[text()='My Bookings']"
+add_funds_button = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/add_funds']"
+add_funds_verify = "//h3[text()='Add Funds']"
+profile_button = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/profile']"
+profile_verify = "//h3[text()='Profile Information']"
+logout_button = "//a[@class=' waves-effect' and @href='https://phptravels.net/account/logout']"
 
 # with is context management, we use this keyword so python automatically starts and closes the playwright context which is created when we call the sync_playwright.start() method.
 with sync_playwright() as p:
@@ -31,39 +31,39 @@ with sync_playwright() as p:
   page = browser.new_page()
   
   # open the site
-  page.goto(URL)
+  page.goto(url)
   
   # enter the email
-  page.fill(EMAILIDTEXTBOX, EMAILID)
+  page.fill(email_id_textbox, email_id)
   
   # enter the password
-  page.fill(PASSWORDTEXTBOX, PASSWORD)
+  page.fill(password_text_box, password)
   
   # click the submit button
-  page.click(SUBMIT_BUTTON)
+  page.click(submit_button)
   
   # wait until the dashboard loads
-  page.wait_for_url(DASHBOARD_URL)
+  page.wait_for_url(dashboard_url)
   
   # click on the my bookings button
-  page.click(MY_BOOKINGS_BUTTON)
+  page.click(my_bookings_button)
   
   # wait for the page to load 
-  page.wait_for_selector(MY_BOOKINGS_VERIFY)
+  page.wait_for_selector(my_bookings_verify)
 
-  page.click(ADD_FUNDS_BUTTON)
+  page.click(add_funds_button)
   
-  page.wait_for_selector(ADD_FUNDS_VERIFY)
+  page.wait_for_selector(add_funds_verify)
   
-  page.click(PROFILE_BUTTON)
+  page.click(profile_button)
   
-  page.wait_for_selector(PROFILE_VERIFY)
+  page.wait_for_selector(profile_verify)
 
   # click the logout button
-  page.click(LOGOUT_BUTTON)
+  page.click(logout_button)
   
   # verify logout by checking for the email id text box
-  page.wait_for_selector(EMAILIDTEXTBOX)
+  page.wait_for_selector(email_id_textbox)
   
   # make sure to close the page and browser when tests complete
   page.close()
