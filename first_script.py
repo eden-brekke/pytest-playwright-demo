fc7a0b34b393c0e3175c2a704b60956b5229305f
Eden Brekke <eden.lorrai@gmail.com>
2023-01-30 11:24:15 -0800

    add email and password xpaths and targets

--- first_script.py
+++ first_script.py
@@ -4,3 +4,9 @@ URL = 'https://phptravels.net/login'
 DASHBOARD_URL = 'https://phptravels.net/account/dashboard'
 
 
+EMAILIDTEXTBOX = "//input[@placeholder='Email']" # if you control f on the console and search for this xpath on the webpage it will highlight the element which is being targetted.
+EMAILID = "user@phptravels.com"
+PASSWORDTEXTBOX = "//input[@placeholder='Password']"
+PASSWORD = "demouser"
+SUBMIT_BUTTON = "//button[@type='submit']"
+
